486d828f19247473bf615b36515b6b1640bfbf8f
sxrrow <vladislav.tabura@gmail.com>
2024-10-26 21:16:03 +0200

    added console.log for variables

--- data/database.js
+++ data/database.js
@@ -11,6 +11,7 @@ const client = new MongoClient(uri);
 console.log('Trying to connect to db');
 
 try {
+  console.log(dbUser, dbPassword, dbName, clusterAddress);
   await client.connect();
   await client.db(dbName).command({ ping: 1 });
   console.log('Connected successfully to server');
